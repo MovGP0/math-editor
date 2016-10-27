51b75ee5d59a1280b7ea856bacec1d2c91149d01
Johann Dirry <movgp0@gmail.com>
2016-10-27 12:55:46 +0200

    adding uwp project

---
+++ Editor.Uwp/AboutPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+
+namespace Editor
+{
+    public partial class AboutWindow
+    {
+        public static readonly DependencyProperty VersionProperty = DependencyProperty.Register(
+            nameof(Version), typeof(string), typeof(AboutWindow), new PropertyMetadata(default(string)));
+
+        public string Version
+        {
+            get { return (string) GetValue(VersionProperty); }
+            set { SetValue(VersionProperty, value); }
+        }
+
+        public AboutWindow()
+        {
+            InitializeComponent();
+            Version = GetVersionString();
+
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+        }
+
+        private static string GetVersionString()
+        {
+            var versionAsString = typeof(AboutWindow).GetTypeInfo().Assembly.GetName().Version.ToString(4);
+            return $"Math Editor v.{versionAsString}";
+        }
+    }
+}
